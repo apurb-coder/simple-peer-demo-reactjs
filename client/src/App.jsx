f3735c79c02d8bf1a371e789459296a86ede94f7
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-09-11 01:12:48 +0530

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 import { io } from "socket.io-client";
-// import Peer from "simple-peer";
+import Peer from "simple-peer";
 
 const App = () => {
   const localVideo = useRef(null);
+
+  const socket = useMemo(() => {
+    return io("http://localhost:8000", {
+      reconnectionAttempts: Infinity,
+      reconnectionDelay: 1000,
+    });
+  }, []);
+
+  useEffect(() => {
+    if(!socket) return;
+    socket.on("YourSocketId", ({socketID}) =>{
+      console.log("Your socket ID is: ", socketID);
+    });
+    socket.on("AllConnectedUsers", ({users})=>{
+      console.log("All connected users: ");
+      console.log(users);
+      
+    });
+    return ()=>{
+      socket.off("YourSocketId");
+    }
+  }, [socket]);
   useEffect(() => {
     const getLocalVideoStream = async () => {
       try {
@@ -20,19 +42,30 @@ const App = () => {
     };
     getLocalVideoStream();
   }, []);
+  const handleRequestUsers = () => {
+    socket.emit("getAllConnectedUsers");
+  };
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen text-blue-500">
       <h2 className="mb-6 ">Video Call demo using simple-peer</h2>
       <div className="flex">
         <div className="flex flex-col justify-center items-center">
           <p> My Video</p>
-          <video id="localVideo" autoPlay playsInline ref={localVideo} className="w-80 h-80" />
+          <video
+            id="localVideo"
+            autoPlay
+            playsInline
+            ref={localVideo}
+            className="w-80 h-80"
+          />
         </div>
         <div className="flex flex-col justify-center items-center">
           <p> Remote Video</p>
           <video id="remoteVideo" autoPlay playsInline />
         </div>
       </div>
+      <button onClick={handleRequestUsers} className=" bg-blue-300 px-4 py-2 rounded-lg">Show Users List</button> 
+
     </div>
   );
 };
---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  },
+});
+let Users = []; // [ socket_id1, socket_id2,socket_id3,...]
+io.on("connection", (socket) => {
+  console.log("New User connected");
+  socket.emit("YourSocketId", {socketID: socket.id});
+  Users.push(socket.id);
+  socket.on("getAllConnectedUsers",()=>{
+    const userData = Users.filter(user=> user!==socket.id);
+    socket.emit("AllConnectedUsers", { users: userData });
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
